81c458384167a9214a5f07a69144429fc30e029d
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-07-27 15:34:11 +0800

    添加 mybatisPlus 功能

--- src/main/java/com/chen/myhr/MyhrApplication.java
+++ src/main/java/com/chen/myhr/MyhrApplication.java
@@ -1,12 +1,16 @@
 package com.chen.myhr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Chen
  */
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.chen"})
+@MapperScan("com.chen.myhr.mapper")
 public class MyhrApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/chen/myhr/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.chen.myhr.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Chen
+ * @description 配置 swagger
+ * @create 2021-07-27
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    // http://localhost:7000/swagger-ui.html
+
+    @Bean
+    public Docket webApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+
+    }
+
+    private ApiInfo webApiInfo(){
+        return new ApiInfoBuilder()
+                .title("API文档")
+                .description("人事管理系统的 api 文档")
+                .version("1.0")
+                .build();
+    }
+}
