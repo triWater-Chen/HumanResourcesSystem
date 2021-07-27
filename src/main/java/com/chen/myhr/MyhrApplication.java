83d6b47dd2c3c50ea546e57f0dcf1ab0271e7446
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-07-27 12:59:42 +0800

    init

---
+++ src/main/java/com/chen/myhr/MyhrApplication.java
@@ -0,0 +1,16 @@
+package com.chen.myhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Chen
+ */
+@SpringBootApplication
+public class MyhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyhrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chen/myhr/MyhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.myhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
