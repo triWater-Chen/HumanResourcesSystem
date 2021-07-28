5c7bbc8742a5ab237908bbf131002eea016f4ebf
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-07-28 20:28:09 +0800

    添加测试文件

---
+++ src/test/java/com/chen/myhr/MyTest.java
@@ -0,0 +1,16 @@
+package com.chen.myhr;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Chen
+ * @description 测试
+ * @create 2021-07-28
+ */
+public class MyTest {
+
+    @Test
+    public void test() {
+
+    }
+}
