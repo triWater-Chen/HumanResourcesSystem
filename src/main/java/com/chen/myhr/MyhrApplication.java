52dd1e47552ad5ce40e9b43fcb156e0d9ec52f26
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-07-28 19:45:03 +0800

    添加控制台打印 swagger 地址

--- src/main/java/com/chen/myhr/MyhrApplication.java
+++ src/main/java/com/chen/myhr/MyhrApplication.java
@@ -1,6 +1,8 @@
 package com.chen.myhr;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,8 +15,11 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.chen.myhr.mapper")
 public class MyhrApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MyhrApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MyhrApplication.class, args);
+        LOG.info("swagger 文档地址：http://localhost:7000/swagger-ui.html");
     }
 
 }
