0d7a176290c7c0aff592884f0f77bbcd6a0bf518
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-02 22:49:05 +0800

    完善职位剩下功能 -- 修改和删除尚未实现

--- src/main/java/com/chen/myhr/controller/PositionController.java
+++ src/main/java/com/chen/myhr/controller/PositionController.java
@@ -1,17 +1,79 @@
 package com.chen.myhr.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.chen.myhr.bean.Position;
+import com.chen.myhr.common.utils.Result;
+import com.chen.myhr.service.PositionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Chen
  * @since 2021-07-28
  */
+@Api(tags = {"职位管理"})
 @RestController
-@RequestMapping("/myhr/position")
+@RequestMapping("/system/basic/position")
 public class PositionController {
 
+    @Resource
+    PositionService positionService;
+
+    @ApiOperation("获取所有职位")
+    @GetMapping("/list")
+    public Result getAllPositions() {
+
+        List<Position> positions = positionService.list();
+        return Result.done().data("list", positions);
+    }
+
+    @ApiOperation("添加职位")
+    @PostMapping("/add")
+    public Result addPositions(String name) {
+
+        Position position = new Position();
+        position.setName(name);
+        position.setEnabled(true);
+        if (positionService.save(position)) {
+            return Result.done().message("添加成功");
+        }
+        return Result.error().message("添加失败");
+    }
+
+    @ApiOperation("修改职位")
+    @PostMapping("/update")
+    public Result updatePositions(@RequestBody Position position) {
+
+        if (positionService.updateById(position)) {
+            return Result.done().message("修改成功");
+        }
+        return Result.error().message("修改失败");
+    }
+
+    @ApiOperation("删除职位（未使用）")
+    @PostMapping("/remove/{id}")
+    public Result removePositions(@PathVariable int id) {
+
+        if (positionService.removeById(id)) {
+            return Result.done().message("删除成功");
+        }
+        return Result.error().message("删除失败");
+    }
+
+    @ApiOperation("批量删除职位")
+    @PostMapping("/removeBatch")
+    public Result removeBatchPositions(@RequestBody Map ids) {
+
+        System.out.println(ids);
+//        if (positionService.removeByIds(ids)) {
+//            return Result.done().message("删除成功");
+//        }
+        return Result.error().message("删除失败");
+    }
 }
 
