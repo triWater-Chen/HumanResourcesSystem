a9fd1b9980435a26592e9b7dc966acc3a84cfa9d
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-03 19:25:23 +0800

    实现自动注入

--- src/main/java/com/chen/myhr/bean/Joblevel.java
+++ src/main/java/com/chen/myhr/bean/Joblevel.java
@@ -1,10 +1,13 @@
 package com.chen.myhr.bean;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -28,9 +31,10 @@ public class Joblevel implements Serializable {
     private String name;
 
     @TableField("titleLevel")
-    private String titlelevel;
+    private String titleLevel;
 
-    @TableField("createDate")
+    @TableField(fill = FieldFill.INSERT, value = "createDate")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
     private Date createdate;
 
     private Boolean enabled;
