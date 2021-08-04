488d442ffc27a36863ce9f4dae1605a168465d1b
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-04 18:52:49 +0800

    配置 MybatisPlus 插件 配置分页功能

--- src/main/java/com/chen/myhr/config/mybatis/MyMetaObjectHandler.java
+++ src/main/java/com/chen/myhr/config/mybatis/MyMetaObjectHandler.java
@@ -1,4 +1,4 @@
-package com.chen.myhr.common.utils;
+package com.chen.myhr.config.mybatis;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
