df30e6103789a101deca3183d00da1b137133670
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-09 21:59:40 +0800

    添加自定义常量标识

---
+++ src/main/java/com/chen/myhr/common/utils/CommonConstants.java
@@ -0,0 +1,19 @@
+package com.chen.myhr.common.utils;
+
+/**
+ * @author Chen
+ * @description 自定义常量标识
+ * @create 2021-08-09
+ */
+public class CommonConstants {
+
+    /**
+     * sql 成功
+     */
+    public static final String SQL_SUCCESS = "OK";
+
+    /**
+     * 标识：sql 中出现不符合查询条件的情况
+     */
+    public static final String STATUS = "OUT";
+}
