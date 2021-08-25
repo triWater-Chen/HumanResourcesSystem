124cb6941bc018ba0860e1edce492aad7ad2a814
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-26 02:40:39 +0800

    调整工资账套实体类字段

--- service/src/main/java/com/chen/myhr/bean/Salary.java
+++ service/src/main/java/com/chen/myhr/bean/Salary.java
@@ -1,10 +1,13 @@
 package com.chen.myhr.bean;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -26,50 +29,51 @@ public class Salary implements Serializable {
 
     @ApiModelProperty(value = "基本工资")
     @TableField("basicSalary")
-    private Integer basicsalary;
+    private Integer basicSalary;
 
     @ApiModelProperty(value = "奖金")
     private Integer bonus;
 
     @ApiModelProperty(value = "午餐补助")
     @TableField("lunchSalary")
-    private Integer lunchsalary;
+    private Integer lunchSalary;
 
     @ApiModelProperty(value = "交通补助")
     @TableField("trafficSalary")
-    private Integer trafficsalary;
+    private Integer trafficSalary;
 
     @ApiModelProperty(value = "应发工资")
     @TableField("allSalary")
-    private Integer allsalary;
+    private Integer allSalary;
 
     @ApiModelProperty(value = "养老金基数")
     @TableField("pensionBase")
-    private Integer pensionbase;
+    private Integer pensionBase;
 
     @ApiModelProperty(value = "养老金比率")
     @TableField("pensionPer")
-    private Float pensionper;
+    private Float pensionPer;
 
     @ApiModelProperty(value = "启用时间")
-    @TableField("createDate")
+    @TableField(fill = FieldFill.INSERT, value = "createDate")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
     private Date createdate;
 
     @ApiModelProperty(value = "医疗基数")
     @TableField("medicalBase")
-    private Integer medicalbase;
+    private Integer medicalBase;
 
     @ApiModelProperty(value = "医疗保险比率")
     @TableField("medicalPer")
-    private Float medicalper;
+    private Float medicalPer;
 
     @ApiModelProperty(value = "公积金基数")
     @TableField("accumulationFundBase")
-    private Integer accumulationfundbase;
+    private Integer accumulationFundBase;
 
     @ApiModelProperty(value = "公积金比率")
     @TableField("accumulationFundPer")
-    private Float accumulationfundper;
+    private Float accumulationFundPer;
 
     private String name;
 
