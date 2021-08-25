bdbf02dff91dca3e18e9eb200713cdd0517a1d50
Chen <49113016+triWater-Chen@users.noreply.github.com>
2021-08-26 02:41:03 +0800

    实现工资账套的查询

--- service/src/main/java/com/chen/myhr/controller/SalaryController.java
+++ service/src/main/java/com/chen/myhr/controller/SalaryController.java
@@ -1,17 +1,36 @@
 package com.chen.myhr.controller;
 
-
+import com.chen.config.result.Result;
+import com.chen.myhr.bean.Salary;
+import com.chen.myhr.service.SalaryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author Chen
  * @since 2021-07-28
  */
+@Api(tags = {"工资账套管理"})
 @RestController
-@RequestMapping("/com/chen/myhr/salary")
+@RequestMapping("/salary/sob")
 public class SalaryController {
 
+    @Resource
+    SalaryService salaryService;
+
+    @ApiOperation("获取所有职称")
+    @GetMapping("/list")
+    public Result getAllSalarySobs() {
+
+        List<Salary> sobs = salaryService.list();
+        return Result.done().data("list", sobs);
+    }
 }
 
